b9ea30df05af6a0451524da43bbd3073a44f9122
Artiom Yashkevich <yashkevich6235@gmail.com>
2021-12-17 17:16:07 +0300

    Commit Java. Fundamentals

--- JavaFundamentals/src/main/java/Main.java
+++ JavaFundamentals/src/main/java/Main.java
@@ -2,12 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);                  // Program 1
         System.out.println("Введите своё имя!");
         String userField = scan.nextLine();
         System.out.println("Приветствую, " + userField);
 
-        System.out.println("Требуется ввести число соответствующего месяца");
+        StringBuilder reverseOrder = new StringBuilder(userField);     // Program 2
+        reverseOrder.reverse();
+        System.out.println("Аргументы командной строки в обратном порядке " + "'" + reverseOrder + "'");
+
+        int a = (int) (Math.random() * 10);                     // Program 3
+        System.out.println(a);
+        System.out.print(a);
+
+        System.out.println("\nВведите первое число");           // Program 4
+        int x = scan.nextInt();
+        System.out.println("Введите второе число");
+        int y = scan.nextInt();
+        System.out.println("Сумма двух чисел = " + (x + y));
+
+        System.out.println("Требуется ввести число соответствующего месяца");       // Program 5
         switch (scan.nextInt()){
             case 1:
                 System.out.println("Январь");break;
