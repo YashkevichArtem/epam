63b3ff952309fd97c89028dd7c2af97f01a6e091
Artiom Yashkevich <yashkevich6235@gmail.com>
2021-12-20 00:29:19 +0300

    commit java fundamentals

--- JavaFundamentals/src/main/java/OptionalTaskOne.java
+++ JavaFundamentals/src/main/java/OptionalTaskOne.java
@@ -2,39 +2,41 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 
 public class OptionalTaskOne {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
 
         while (true) {
             System.out.println("Введите число и нажмите Enter. Если желаете окончить поиск, оставьте поле пустым и нажмите Enter.");
             String s = reader.readLine();
             if (s.isEmpty()) break;
+            int toInt = Integer.parseInt(s);
 
-            list.add(s);
+            list.add(toInt);
         }
-
-        String longNumber = list.get(0);
-        String shortNumber = list.get(0);
-        for(int i = 1; i < list.size(); i++){
-            if(list.get(i).length() >= longNumber.length()){
-                longNumber = list.get(i);
-            }else{
-                shortNumber = list.get(i);
-            }
-        }
-        System.out.println("Самое короткое и маленькое число: " + shortNumber + ". Это " + shortNumber.length() + " значное число." );
-        System.out.println("Самое длинное и большое число: " + longNumber + ". Это " + longNumber.length() + " значное число." );
 
         Collections.sort(list);
+
+        int longNumber = list.get(0);
+        int shortNumber = list.get(0);
+        for (Integer integer : list) {
+            if (integer >= longNumber) {
+                longNumber = integer;
+            } else {
+                shortNumber = integer;
+            }
+        }
+        String longNumberLength = Integer.toString(longNumber);
+        String shortNumberLength = Integer.toString(shortNumber);
+        System.out.println("Самое короткое и маленькое число: " + shortNumber + ". Это " + shortNumberLength.length() + " значное число." );
+        System.out.println("Самое длинное и большое число: " + longNumber + ". Это " + longNumberLength.length() + " значное число." );
+
         System.out.print("Числа в порядке возрастания значений их длины: ");
-        for(String all : list){
-            System.out.print(all + ";");
+        for(Integer all : list){
+            System.out.print(all + "; ");
         }
     }
 }
